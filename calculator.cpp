4d5250cc24a9c677b882795e3d53920fe13dff31
Rehman Ali <56971509+rehmanalidev@users.noreply.github.com>
2019-10-25 00:35:48 -0700

    Added the comments

--- calculator.cpp
+++ calculator.cpp
@@ -1,19 +1,20 @@
+/* This is a simple C++ programe for performing basic Arithmetic operation like addition, subtraction, multiplication and division
 #include <iostream>
 
 using namespace std;
-float operation(float num1, float num2, char oper);
+float operation(float num1, float num2, char oper);			// function for computing operation
 int main()
 {
-	float n1, n2, result;
-	char oper;
+	float n1, n2, result;				// n1, n2 as operands and result of type float to store answer
+	char oper;					// char operator for operator 
 	cout << "The program is calculator of basic calculations ";
 	cout << "\nEnter 1st Operand : ";
-	cin >> n1;
+	cin >> n1;						// input the first operand
 	cout << "Enter 2nd Operand : ";
-	cin >> n2;
+	cin >> n2;						// input the 2nd operand		
 	cout << "Enter operator : ";
-	cin >> oper;
-	result = operation(n1, n2, oper);
+	cin >> oper;						// iput the operator 
+	result = operation(n1, n2, oper);			// store the result of function operation in result variable
 
 	cout << "This result of integer expression is : " << result << endl;
 	system("pause");
